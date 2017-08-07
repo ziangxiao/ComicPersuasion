1cd6a291cebaab946aea8e9c7dd43600379c7fb6
Ziang Xiao <ziangxiao5135@gmail.com>
2017-08-07 12:01:29 -0500

    Generator Update

--- customize.js
+++ customize.js
@@ -1,22 +1,28 @@
 // 3 parameters controlling the different levels of elements
-var gest = 0;
-var dist = 0;
-var shad = 1;
+var gest = 2;
+var dist = 1;
+var shad = 0;
 function setActor1(){ // set the position and gesture of actor 1
 	var act1 = document.getElementById('actor1');
-	if(gest==0){
+	if(gest==0){//Neutral
 		act1.setAttribute('t', 'translate(94,19) rotate(-2)');
-		act1.setAttribute('pose', '-11,9|-9,120|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|5,86|22,81');
+		act1.setAttribute('pose', '-11,9|-5,117|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|-1,59');
+		//Old version
+		//act1.setAttribute('pose', '-11,9|-9,120|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|5,86|22,81');
 	}
 	else if(gest==1){
 		act1.setAttribute('t', 'translate(71,19) rotate(-2)');
-		act1.setAttribute('pose', '-11,9|1,114|-9,98|-9,88|-15,79|-11,59|-16,34|-21,9|-6,34|-1,9|-19,78|-19,58|4,82|18,110');
+		//Negative
+		//act1.setAttribute('pose', '-11,9|1,114|-9,98|-9,88|-15,79|-11,59|-16,34|-21,9|-6,34|-1,9|-19,78|-19,58|4,82|18,110');
+		//Positive
+		act1.setAttribute('pose', '-11,9|-20,121|-11,99|-11,89|-2,76|-1,54|-16,32|-21,7|15,40|20,15|-32,81|-35,57|11,106|12,128');
 	}
 	else{ //gest = 2
 		act1.setAttribute('t', 'translate(71,19) rotate(-2)');
-		act1.setAttribute('pose', '-11,9|13,90|-3,80|-5,60|-9,66|-8,46|14,29|-11,21|8,39|-16,29|-13,68|-12,52|10,64|25,80');
-		//act1.setAttribute('t', 'translate(71,19) rotate(-2)');
-		//act1.setAttribute('pose', '-11,9|-1,114|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|13,83');
+		//Negative
+		//act1.setAttribute('pose', '-11,9|13,90|-3,80|-5,60|-9,66|-8,46|14,29|-11,21|8,39|-16,29|-13,68|-12,52|10,64|25,80');
+		//Positive
+		act1.setAttribute('pose', '-11,9|-17,116|-11,99|-4,92|5,78|-1,58|-9,40|-28,29|23,59|28,40|-33,95|-54,114|8,111|17,136');
 	}
 	if(dist==0){ // set position
 		if(gest==2)
@@ -40,25 +46,25 @@ function setActor1(){ // set the position and gesture of actor 1
 function setActor2(){ // set the position and gesture of actor 2
 	var act2 = document.getElementById('actor2');
 	if(gest==0){
-		act2.setAttribute('t', 'translate(159,15)');
-		act2.setAttribute('pose', '28,1|30,109|28,91|28,81|28,74|28,54|18,34|18,4|33,29|38,4|23,71|18,51|38,71|38,51');
+		act2.setAttribute('t', 'translate(159,10)');
+		act2.setAttribute('pose', "28,1|30,109|28,91|28,81|28,71|28,51|18,31|18,1|33,26|38,1|17,76|-4,71|38,71|38,51");
 	}
 	else if(gest==1){
-		act2.setAttribute('t', 'translate(159,15)');
-		act2.setAttribute('pose', '28,1|23,108|28,91|28,81|28,74|28,54|18,34|18,4|33,29|38,4|23,71|18,51|38,71|38,51');
+		act2.setAttribute('t', 'translate(169,10)');
+		act2.setAttribute('pose', "28,1|30,109|28,91|28,81|28,71|28,51|18,31|18,1|33,26|38,1|17,76|-4,71|38,71|38,51");
 	}
 	else{
-		act2.setAttribute('t', 'translate(159,15)');
-		act2.setAttribute('pose', '28,1|18,107|28,91|28,81|28,74|28,54|18,34|18,4|33,29|38,4|19,73|19,53|38,71|38,51');
+		act2.setAttribute('t', 'translate(169,10)');
+		act2.setAttribute('pose', "28,1|30,109|28,91|28,81|28,71|28,51|18,31|18,1|33,26|38,1|17,76|-4,71|38,71|38,51");
 	}
 	if(dist==0){ //set position
-		act2.setAttribute('t', 'translate(129,18)');
+		act2.setAttribute('t', 'translate(139,18)');
 	}
 	else if(dist==1){
-		act2.setAttribute('t', 'translate(146,17) rotate(-1)');
+		act2.setAttribute('t', 'translate(156,17) rotate(-1)');
 	}
 	else{
-		act2.setAttribute('t', 'translate(169,18) rotate(-1)');
+		act2.setAttribute('t', 'translate(179,18) rotate(-1)');
 	}
 }
 function render(){
@@ -77,16 +83,18 @@ function render(){
   			}
   			//alert("inside!!");
   		}
-		
-	//Random switch the position of the text and comic
-	if(Math.random() >= 0.5)
-	{
-		switchTextComic()
-	}
+			shad+=1;
 }
 
 function switchTextComic(){
 	var elem = document.getElementById("middlePart");
 	elem.parentNode.insertBefore(elem,elem.parentNode.firstElementChild);
 	elem.parentNode.insertBefore(elem.parentNode.lastElementChild,elem.parentNode.firstElementChild);
+}
+
+function comicPosition(){
+	if(Math.random() >= 0.5)
+	{
+		switchTextComic()
+	}
 }
