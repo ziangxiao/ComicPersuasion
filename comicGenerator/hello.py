5fc962acb7ecdacd94557ed96906aed6e7f5b165
wangxr14 <wangxr1108@126.com>
2017-08-22 18:05:03 -0500

    Add back-end

--- comicGenerator/hello.py
+++ comicGenerator/hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import Response
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 	
 @app.route("/xkcd/")
 def xkcd():
